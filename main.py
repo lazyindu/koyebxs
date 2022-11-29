e72e7825dd28bb5cdf61e0777a51144fe6c0c581
indu <70087467+lazyindu@users.noreply.github.com>
2022-11-29 16:37:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from TeamTeleRoid.forcesub import ForceSub
 
 # Bot Client for Inline Search
 Bot = Client(
-    session_name=Config.BOT_SESSION_NAME,
+    name=Config.BOT_SESSION_NAME,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN
