3d6105eb8fd3cdcb0825175c4adbe0718364559f
indu <70087467+lazyindu@users.noreply.github.com>
2022-11-29 19:10:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/PredatorHackerzZ/MessageSearchBot.git /MessageSearchBot
 cd /MessageSearchBot
-pip3 install -r requirements.txt
+pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
